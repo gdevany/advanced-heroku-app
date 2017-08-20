4b52fb7154537689ed823daf4dbab2887e209aee
Greg Devany <greg.devany@yahoo.com>
2017-08-19 20:25:06 -0500

    Working on ShowCoupon

--- client/src/state.js
+++ client/src/state.js
@@ -25,11 +25,41 @@ export default {
   ],
   coupons: [
     {
-      id: 0,
+      id: 1,
       bizName: "Mcdonalds",
       bizLogo: "",
       heading: "Buy 1 BigMac, Get One Free",
       couponDesc: "Buy 1 Bigmac and receive 1 BigMac free!",
+      restrictions: "One coupon per day",
+      searchWords: ["Fast Food"],
+      bizAddress:
+        {
+          streetAndNum: "1234 Main St",
+          city: "Austin",
+          zip: 78701
+        }
+    },
+    {
+      id: 2,
+      bizName: "Burger King",
+      bizLogo: "",
+      heading: "Buy 1 Whopper, Get One Free",
+      couponDesc: "Buy 1 Whopper and receive 1 Whopper free!",
+      restrictions: "One coupon per day",
+      searchWords: ["Fast Food"],
+      bizAddress:
+        {
+          streetAndNum: "1234 Oak St",
+          city: "Austin",
+          zip: 78701
+        }
+    },
+    {
+      id: 3,
+      bizName: "Chick-Fil-A",
+      bizLogo: "",
+      heading: "Buy 1 Chick-Fil-A sandwich, Get One Free",
+      couponDesc: "Buy 1 Chick-Fil-A sandwich and receive 1 Chick-Fil-A sandwich free!",
       restrictions: "One coupon per day",
       searchWords: ["Fast Food"],
       bizAddress:
