65ffed22a8f20def0053810e197bd586b0b58255
Greg Devany <greg.devany@yahoo.com>
2017-08-19 22:29:27 -0500

    Working on ShowCoupon

--- client/src/components/ShowCoupons.js
+++ client/src/components/ShowCoupons.js
@@ -12,25 +12,45 @@ function ShowCoupons(props) {
 
   var couponDiv = "";
 
+//   if (props.searchCoupons !== "") {
+//     couponDiv = filterCoupons.map((coupon) => {
+//       return <div key={coupon.id}>{couponFormat(coupon)}</div>
+//     })
+//   } else couponDiv = <div></div>
+//
+// function couponFormat(coupon) {
+//   return (<div className="coupon">
+//     <h2>{coupon.bizName}</h2>
+//     <div>{coupon.bizLogo}</div>
+//     <div>{coupon.bizAddress.streetAndNum}</div>
+//     <div>{coupon.bizAddress.city}</div>
+//     <div>{coupon.bizAddress.zip}</div>
+//     <div>{coupon.heading}</div>
+//     <div>{coupon.couponDesc}</div>
+//     <div>{coupon.restrictions}</div>
+//     </div>
+//   )
+
   if (props.searchCoupons !== "") {
     couponDiv = filterCoupons.map((coupon) => {
-      return <div key={coupon.id} className="coupon">{couponFormat(coupon)}</div>
+      return (
+        <div key={coupon.id} className="coupon container">
+          <div className="row">
+            <h5>{coupon.bizName}</h5>
+            <div>{coupon.bizLogo}</div>
+          </div>
+          <div className="row">
+            <div>{coupon.bizAddress.streetAndNum}</div>
+            <div>{coupon.bizAddress.city}</div>
+            <div>{coupon.bizAddress.zip}</div>
+          </div>
+          <div>{coupon.heading}</div>
+          <div>{coupon.couponDesc}</div>
+          <div>{coupon.restrictions}</div>
+        </div>
+      )
     })
   } else couponDiv = <div></div>
-
-function couponFormat(coupon) {
-  return (<div>
-    <h2>{coupon.bizName}</h2>
-    <div>{coupon.bizLogo}</div>
-    <div>{coupon.bizAddress.streetAndNum}</div>
-    <div>{coupon.bizAddress.city}</div>
-    <div>{coupon.bizAddress.zip}</div>
-    <div>{coupon.heading}</div>
-    <div>{coupon.couponDesc}</div>
-    <div>{coupon.restrictions}</div>
-    </div>
-  )
-}
 
   return (
     <div>
