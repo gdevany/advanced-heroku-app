38fb34f07d0c2f798c5f9479cd023fa74c15bc2d
Greg Devany <greg.devany@yahoo.com>
2017-08-21 14:55:12 -0500

    Improvements to coupon

--- client/src/components/ShowCoupons.js
+++ client/src/components/ShowCoupons.js
@@ -18,18 +18,25 @@ function ShowCoupons(props) {
     couponDiv = filterCoupons.map((coupon) => {
       return (
         <div key={coupon.id} className="coupon container">
-          <div className="row">
-            <h5>{coupon.bizName}</h5>
+
+          <div className="couponRowBizName">
+            <div className="bigText">{coupon.bizName}</div>
+            <div className="tightAddress">
+              {coupon.bizAddress.streetAndNum}
+              {coupon.bizAddress.city}
+              {coupon.bizAddress.zip}
+            </div>
             <div>{coupon.bizLogo}</div>
           </div>
-          <div className="row">
-            <div>{coupon.bizAddress.streetAndNum}</div>
-            <div>{coupon.bizAddress.city}</div>
-            <div>{coupon.bizAddress.zip}</div>
+
+          <div className="row couponRowInfo">
+            <div className="tightLines">Buy One</div>
+            <div className="biggerText">{coupon.heading}</div>
+            <div className="tightLines">Get One</div>
+            <div className="bigText">Free</div>
+            <div>{coupon.couponDesc}</div>
+            <div>{coupon.restrictions}</div>
           </div>
-          <div>{coupon.heading}</div>
-          <div>{coupon.couponDesc}</div>
-          <div>{coupon.restrictions}</div>
         </div>
       )
     })
