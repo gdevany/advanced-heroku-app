5bd38011450308887ea874b08f91335b2385d3ef
Greg Devany <greg.devany@yahoo.com>
2017-09-12 23:50:49 -0500

    Css

--- client/src/components/Geolocated.js
+++ client/src/components/Geolocated.js
@@ -5,38 +5,36 @@ const { geo2zip } = require('geo2zip')
 
 class ZipcodeSetter extends React.Component {
 
-    setTheZip = (lat, lon) => {
-      const here = {
-        latitude: lat,
-        longitude: lon
-      }
-
-      geo2zip(here)
-        .then(zip => {
-          const zipIt = JSON.stringify(zip);
-          this.props.setZip(zip);
-        })
-
-      console.log(this.props.zip);
+  setTheZip = (lat, lon) => {
+    const here = {
+      latitude: lat,
+      longitude: lon
     }
 
-    render() {
+    geo2zip(here)
+      .then(zip => {
+        // const zipIt = JSON.stringify(zip);
+        this.props.setZip(zip);
+      })
+  }
 
-    return !this.props.isGeolocationAvailable
-      ? <div>Your browser does not support Geolocation</div>
-      : !this.props.isGeolocationEnabled
-        ? <div>Geolocation is not enabled</div>
-        : this.props.coords
-          ? <div className="margin30Bottom">
-              <div>
-              {this.setTheZip(this.props.coords.latitude,this.props.coords.longitude)}
-              Your current zipcode
-              </div>
-              <span className="zipBox bigText">
-                {this.props.zip}
-              </span>
+  render() {
+
+  return !this.props.isGeolocationAvailable
+    ? <div>Your browser does not support Geolocation</div>
+    : !this.props.isGeolocationEnabled
+      ? <div>Geolocation is not enabled</div>
+      : this.props.coords
+        ? <div className="margin30Bottom">
+            <div>
+            {this.setTheZip(this.props.coords.latitude,this.props.coords.longitude)}
+            Your current zipcode
             </div>
-          : <div>Getting the location data&hellip; </div>;
+            <span className="zipBox">
+              {this.props.zip}
+            </span>
+          </div>
+        : <div>Getting the location data&hellip; </div>;
   }
 }
 // <div><small>latitude{this.props.coords.latitude}</small></div>
