6c3dcd9fb428403c2340f441fc344937645e2096
Greg Devany <greg.devany@yahoo.com>
2017-09-28 12:01:53 -0500

    Fixing npm zips asynchronous issue. Fixed

--- client/src/components/Geolocated.js
+++ client/src/components/Geolocated.js
@@ -1,57 +1,54 @@
 import React from 'react';
 import {geolocated} from 'react-geolocated';
-// const { geo2zip } = require('geo2zip');
-// var gps = require('gps2zip');
 const zips = require('zips');
 
 
 class ZipcodeSetter extends React.Component {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     myZip: 0
-  //   }
-  // }
+  constructor() {
+    super();
+    this.state = {
+      myZip: 0,
+      loading: false
+    }
+  }
 
+  componentWillMount() {
+    // this.setTheZip(myZip);
+    this.setState({ loading: true });
+    this.loadData()
+    .then((myZip) => {
+      this.setState({
+        myZip: myZip,
+        loading: false
+      });
+    });
+  }
 
-  // const zipView = "";
-  // if (this.props.isGeolocationAvailable &&
-  //   this.props.isGeolocationEnabled &&
-  //   this.props.coords) {
-  //     const myLocation = (zips.getByLocation(
-  //       this.props.coords.latitude,
-  //       this.props.coords.longitude));
-  //     const myZip = myLocation.zip;
-  //     zipView = myZip;
-  //   }
+  loadData = () => {
+    var promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (this.props.isGeolocationAvailable && this.props.isGeolocationEnabled && this.props.coords) {
+            var myLocation = (zips.getByLocation(
+              this.props.coords.latitude,
+              this.props.coords.longitude));
+            var myZip = myLocation.zip;
+            console.log(myZip);
+            this.setTheZip(myZip);
+            console.log(this.props.zip);
+            resolve(myZip);
+          }
+      }, 5000);
+    });
+    return promise;
+  }
 
-  // setTheZip = (lat, lon) => {
-    // const here = {
-    //   latitude: lat,
-    //   longitude: lon
-    // }
-    debugger;
-    //
-    // const x = zips.getByLocation(lat,lon);
-    // console.log(x.zip);
-    // this.setState({myZip:x.zip});
-    // this.props.setZip(x.zip);
-
-    // geo2zip(here)
-    //   .then(zip => {
-    //     this.props.setZip(zip);
-    //   })
-  // }
+  setTheZip = (myZip) => {
+    console.log('here');
+    this.props.setZip(myZip)
+  }
 
   render() {
-    var zipView = "";
-    if (this.props.isGeolocationAvailable && this.props.isGeolocationEnabled && this.props.coords) {
-        var myLocation = (zips.getByLocation(
-          this.props.coords.latitude,
-          this.props.coords.longitude));
-        var myZip = myLocation.zip;
-        zipView = myZip;
-      }
+
   return !this.props.isGeolocationAvailable
     ? <div>Your browser does not support Geolocation</div>
     : !this.props.isGeolocationEnabled
@@ -59,7 +56,6 @@ class ZipcodeSetter extends React.Component {
       : this.props.coords
         ? <div className="margin30Bottom">
             <div>
-            {zipView}
             Your current zipcode
             </div>
             <span className="zipBox">
@@ -69,7 +65,75 @@ class ZipcodeSetter extends React.Component {
         : <div>Getting the location data&hellip; </div>;
   }
 }
-//{this.setTheZip(this.props.coords.latitude,this.props.coords.longitude)}
+//
+// // constructor() {
+// //   super();
+// //   this.state = {
+// //     myZip: 0
+// //   }
+// // }
+//
+// // componentWillMount() {
+// //   this.setTheZip(myZip);
+// //
+// // }
+//
+//
+// // setTheZip = (lat, lon) => {
+//   // const here = {
+//   //   latitude: lat,
+//   //   longitude: lon
+//   // }
+//
+//   setTheZip = (myZip) => {
+//     console.log('here');
+//     this.props.setZip(myZip)
+//   }
+//   //
+//   // const x = zips.getByLocation(lat,lon);
+//   // console.log(x.zip);
+//   // this.setState({myZip:x.zip});
+//   // this.props.setZip(x.zip);
+//
+//   // geo2zip(here)
+//   //   .then(zip => {
+//   //     this.props.setZip(zip);
+//   //   })
+// // }
+//
+// render() {
+//   var zipView = "";
+//   if (this.props.isGeolocationAvailable && this.props.isGeolocationEnabled && this.props.coords) {
+//       var myLocation = (zips.getByLocation(
+//         this.props.coords.latitude,
+//         this.props.coords.longitude));
+//       var myZip = myLocation.zip;
+//       console.log(myZip);
+//       // debugger;
+//       this.setTheZip(myZip);
+//       // debugger;
+//
+//       console.log(this.props.zip);
+//
+//       zipView = myZip;
+//     }
+// return !this.props.isGeolocationAvailable
+//   ? <div>Your browser does not support Geolocation</div>
+//   : !this.props.isGeolocationEnabled
+//     ? <div>Geolocation is not enabled</div>
+//     : this.props.coords
+//       ? <div className="margin30Bottom">
+//           <div>
+//           {zipView}
+//           Your current zipcode
+//           </div>
+//           <span className="zipBox">
+//             {this.props.zip}
+//           </span>
+//         </div>
+//       : <div>Getting the location data&hellip; </div>;
+
+
 export default geolocated({
   positionOptions: {
     enableHighAccuracy: false,
