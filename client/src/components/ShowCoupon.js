ab3ec6cde584bc8f27c6c76d0b2a8a5ed3f6db86
Greg Devany <greg.devany@yahoo.com>
2019-11-04 00:05:14 -0600

    ShowCoupon: Make offers expand and collapse

--- client/src/components/ShowCoupon.js
+++ client/src/components/ShowCoupon.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import BusinessLocationMapped from "./BusinessLocationMapped";
-import googlemapicon from '../../img/googlemapicon.png';
+import googlemapicon from "../../img/googlemapicon.png";
 
 class ShowCoupon extends Component {
   constructor(props) {
-    super();
+    super(props);
     this.state = {
-      showBizMap: false
+      showBizMap: false,
+      expandHeader: false
     };
   }
 
@@ -22,89 +23,167 @@ class ShowCoupon extends Component {
     return "0.3 miles";
   };
 
-  renderCoupon = coupon => {
+  expandIt = () => {
+    this.setState({ expandHeader: !this.state.expandHeader });
+  };
+
+  renderMap = coupon => {
     return (
-      <div className="col-sm-6 col-lg-4">
-        <div className="coupon">
-          <div className="container">
-            <div className="row">
-            {this.state.showBizMap === true ? (
-                  <BusinessLocationMapped
-                    address={coupon.streetAndNum + coupon.zip}
-                    distance={this.calculateDistance()}
-                  />
-                ) : (
-                  <div></div>
-                )}
-              <div className="col-xs-12 couponHeadline">
-               
-                <span className="bizLogo">
-                  <img className="" src={coupon.bizLogo} alt="bizLogo"></img>
-                </span>
-                {this.props.loggedIn === "" ? (
-                  // TODO: calculate distance with geolocation
-                  <span className="distance text-center">
-                    {this.calculateDistance()}
-                  </span>
-                ) : (
-                  <span>{this.props.deleteButton(coupon)}</span>
-                )}
-                <span
-                  className="bizLogo"
-                  onClick={() => this.toggleBizMap()}
-                >
-                  {this.state.showBizMap === false ? (
-                    <img
-                      className=""
-                      src={ googlemapicon }
-                      alt="genMapIcon"
-                    ></img>
-                  ) : (
-                    <div>Close Map</div>
-                  )}
-                </span>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-xs-12 text-center offer">
-                <div className="">Buy One</div>
-                <div className="biggerText">{coupon.heading}</div>
-                <div className="">Get One Free</div>
-              </div>
+      <div className="row">
+        {this.state.expandHeader === true && (
+          <BusinessLocationMapped
+            address={coupon.streetAndNum + coupon.zip}
+            distance={this.calculateDistance()}
+          />
+        )}
+      </div>
+    );
+  };
+
+  renderOfferInfo = coupon => {
+    return (
+      <div>
+        <div className="row">
+          <div className="col-xs-12 text-center">
+            <address>
+              <strong>{coupon.bizName}</strong>
               <br />
-            </div>
-            <div className="row">
-              <div className="col-xs-12 text-center">
-                <address>
-                  <strong>{coupon.bizName}</strong>
-                  <br />
-                  {coupon.streetAndNum} {coupon.city} <br />
-                  <strong>{coupon.zip}</strong>
-                  <br />
-                  <small>{coupon.bizPhone}</small>
-                </address>
-              </div>
-            </div>
-            <div className="row text-left">
-              <div>
-                {"* "}
-                {coupon.couponDesc}
-                {" *"}
-              </div>
-              <div>
-                {"* "}
-                {coupon.restrictions}
-              </div>
-              <div className="">Coupon ID: {coupon._id}</div>
-            </div>
+              {coupon.streetAndNum} {coupon.city} <br />
+              <strong>{coupon.zip}</strong>
+              <br />
+              <small>{coupon.bizPhone}</small>
+            </address>
           </div>
+        </div>
+        <div className="row text-left">
+          <div>
+            {"* "}
+            {coupon.couponDesc}
+            {" *"}
+          </div>
+          <div>
+            {"* "}
+            {coupon.restrictions}
+          </div>
+          <div className="">Offer ID: {coupon._id}</div>
         </div>
       </div>
     );
   };
 
+  renderExpandableOffer = coupon => {
+    return (
+      <div className="couponShort">
+        <div className="container">
+          {/* {this.state.expandHeader === true && this.renderMap(coupon)} */}
+          <div className="row offer" onClick={() => this.expandIt()}>
+            <div className="col-xs-6 couponHeadline">
+              <span className="bizLogo">
+                <img className="" src={coupon.bizLogo} alt="bizLogo"></img>
+              </span>
+              {this.props.loggedIn === "" ? (
+                // TODO: calculate distance with geolocation
+                <span className="distance text-center">
+                  {this.calculateDistance()}
+                </span>
+              ) : (
+                <span>{this.props.deleteButton(coupon)}</span>
+              )}
+            </div>
+            <div className="col-xs-6 text-center">
+              <div className="">Buy One</div>
+              <div className="biggerText">{coupon.heading}</div>
+              <div className="">Get One Free</div>
+            </div>
+            <br />
+          </div>
+          {this.state.expandHeader === true && this.renderMap(coupon)}
+
+          {this.state.expandHeader === true && this.renderOfferInfo(coupon)}
+        </div>
+      </div>
+    );
+  };
+
+  // renderCoupon = coupon => {
+  //   return (
+  //     <div className="col-sm-6 col-lg-4">
+  //       <div className="coupon">
+  //         <div className="container">
+  //           <div className="row">
+  //             {this.state.showBizMap === true ? (
+  //               <BusinessLocationMapped
+  //                 address={coupon.streetAndNum + coupon.zip}
+  //                 distance={this.calculateDistance()}
+  //               />
+  //             ) : (
+  //               <div></div>
+  //             )}
+  //             <div className="col-xs-12 couponHeadline">
+  //               <span className="bizLogo">
+  //                 <img className="" src={coupon.bizLogo} alt="bizLogo"></img>
+  //               </span>
+  //               {this.props.loggedIn === "" ? (
+  //                 // TODO: calculate distance with geolocation
+  //                 <span className="distance text-center">
+  //                   {this.calculateDistance()}
+  //                 </span>
+  //               ) : (
+  //                 <span>{this.props.deleteButton(coupon)}</span>
+  //               )}
+  //               <span className="bizLogo" onClick={() => this.toggleBizMap()}>
+  //                 {this.state.showBizMap === false ? (
+  //                   <img
+  //                     className=""
+  //                     src={googlemapicon}
+  //                     alt="genMapIcon"
+  //                   ></img>
+  //                 ) : (
+  //                   <div>Close Map</div>
+  //                 )}
+  //               </span>
+  //             </div>
+  //           </div>
+  //           <div className="row">
+  //             <div className="col-xs-12 text-center offer">
+  //               <div className="">Buy One</div>
+  //               <div className="biggerText">{coupon.heading}</div>
+  //               <div className="">Get One Free</div>
+  //             </div>
+  //             <br />
+  //           </div>
+  //           <div className="row">
+  //             <div className="col-xs-12 text-center">
+  //               <address>
+  //                 <strong>{coupon.bizName}</strong>
+  //                 <br />
+  //                 {coupon.streetAndNum} {coupon.city} <br />
+  //                 <strong>{coupon.zip}</strong>
+  //                 <br />
+  //                 <small>{coupon.bizPhone}</small>
+  //               </address>
+  //             </div>
+  //           </div>
+  //           <div className="row text-left">
+  //             <div>
+  //               {"* "}
+  //               {coupon.couponDesc}
+  //               {" *"}
+  //             </div>
+  //             <div>
+  //               {"* "}
+  //               {coupon.restrictions}
+  //             </div>
+  //             <div className="">Coupon ID: {coupon._id}</div>
+  //           </div>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   );
+  // };
+
   render() {
-    return <div>{this.renderCoupon(this.props.coupon)}</div>;
+    return <div>{this.renderExpandableOffer(this.props.coupon)}</div>;
   }
 }
 
